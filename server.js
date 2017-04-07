b4d446f8eaddbb337c2da403b592886799de5ae0
Dim4k <kima0012@gmail.com>
2017-04-07 15:31:22 +0200

    better error handle

--- server.js
+++ server.js
@@ -17,20 +17,27 @@ app.use(function(req, res, next) {
 
 app.post('/sendMail',function(req,res,next){
 
-    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-        return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        return res.json({"responseCode": 1, "responseDesc": "Please select captcha"});
     }
 
     let user = null;
 
-    config.users.forEach(function(element,idx,array){
-        if(req.body['website'] === element.website){
-            user = element;
-        }
-        if (idx === array.length - 1 && user == null){
-            res.json({"responseCode" : 2,"responseDesc" : "Website '"+element.website+"' not configured"});
-        }
-    });
+    if (req.body['website'] === undefined) {
+        return res.json({"responseCode": 2, "responseDesc": "Please send a 'website' element"});
+    } else {
+        config.users.forEach(function (element, idx, array) {
+            if (req.body['website'] === element.website) {
+                user = element;
+            }
+            if (idx === array.length - 1 && user == null) {
+                return res.json({
+                    "responseCode": 3,
+                    "responseDesc": "Website '" + req.body['website'] + "' not configured"
+                });
+            }
+        });
+    }
 
     const verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + user.recapthaSecretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
 
@@ -49,23 +56,25 @@ app.post('/sendMail',function(req,res,next){
         });
 
         let mailOptions = {
-            from: req.body['email'], // Sender address
-            to: user.email, // List of receivers
-            subject: 'Messsage from '+req.body['firstname']+' '+req.body['lastname'], // Subject line
-            html: '<br/>Contact email : <br/>'+req.body['email']+'<br/>Message : <br/>'+req.body['message'], // Html body
+            from: req.body['email'],
+            to: user.email,
+            subject: 'Messsage from '+req.body['firstname']+' '+req.body['lastname'],
+            html: '<br/><strong>Contact email : </strong><br/>'
+            +req.body['email']+'<br/>' +
+            '<strong>Message : </strong><br/>' +
+            req.body['message'],
         };
 
         transporter.sendMail(mailOptions, function (err, responseStatus) {
             if(err != null){
-                console.log(err);
+                return res.json({"responseCode" : 4,"responseDesc" : "There was a problem, mail has not been delivered."});
             }
             transporter.close();
         });
-        res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
+        res.json({"responseCode" : 0,"responseDesc" : "Mail sent"});
     });
 });
 
-// lifting the app on port 3000.
 app.listen(3000, function () {
     console.log('ExpressMailer web server listening on port 3000')
 });
